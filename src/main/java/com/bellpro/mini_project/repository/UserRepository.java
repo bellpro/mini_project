0cffbb404fadbf0a0889877d8ec9234e118a7a7e
bellpro <kju1909020@gmail.com>
2021-11-28 12:05:56 +0900

    JPA SQL 설정 (사용자, 게시판, 댓글)

---
+++ src/main/java/com/bellpro/mini_project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bellpro.mini_project.repository;
+
+import com.bellpro.mini_project.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JpaRepository 상속 받아서 사용 <엔티티, ID 자료형>
+// 공통 인터페이스 사용 가능 (save, delete, findById ...)
+public interface UserRepository extends JpaRepository<User, Long> { // User 테이블 연결
+}
