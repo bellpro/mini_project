047f9b7d0b4fa1d4deef76810193a2ac7cfaa65a
bellpro <kju1909020@gmail.com>
2021-11-28 17:14:31 +0900

    생성일자/수정일자 반영

--- src/main/java/com/bellpro/mini_project/MiniProjectApplication.java
+++ src/main/java/com/bellpro/mini_project/MiniProjectApplication.java
@@ -2,7 +2,9 @@ package com.bellpro.mini_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  //생성일자, 수정일자 등의 컬럼에 대해 자동으로 값을 넣어주는 기능
 @SpringBootApplication
 public class MiniProjectApplication {
 
