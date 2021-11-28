442f51e88d67a2869f9b7ffed8a17caf3a9f73cc
bellpro <kju1909020@gmail.com>
2021-11-28 18:34:10 +0900

    사용자 검증 (security)

---
+++ src/main/java/com/bellpro/mini_project/security/UserDetailsImpl.java
@@ -0,0 +1,50 @@
+package com.bellpro.mini_project.security;
+
+import com.bellpro.mini_project.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor    // 의존성 주입, final 필드에 대해 생성자를 생성
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+}
