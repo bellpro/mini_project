fb6b66049a4906c603e1d860a306334f530521cd
bellpro <kju1909020@gmail.com>
2021-11-28 13:26:20 +0900

    사용자 dto 및 dto 생성자 설정

--- src/main/java/com/bellpro/mini_project/repository/UserRepository.java
+++ src/main/java/com/bellpro/mini_project/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository 상속 받아서 사용 <엔티티, ID 자료형>
 // 공통 인터페이스 사용 가능 (save, delete, findById ...)
 public interface UserRepository extends JpaRepository<User, Long> { // User 테이블 연결
+    boolean existsByUsername(String username);  // 아이디 중복 검사
+    boolean existsByEmail(String email);        // 이메일 중복 검사
 }
