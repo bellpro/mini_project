482cfe775797635e1dd47f95ab16eeadd096187c
bellpro <kju1909020@gmail.com>
2021-11-28 14:24:21 +0900

    사용자 컨트롤러 (회원가입), 검증 추가

--- src/main/java/com/bellpro/mini_project/controller/UserController.java
+++ src/main/java/com/bellpro/mini_project/controller/UserController.java
@@ -2,26 +2,49 @@ package com.bellpro.mini_project.controller;
 
 import com.bellpro.mini_project.dto.UserInfoDto;
 import com.bellpro.mini_project.service.UserService;
+import com.bellpro.mini_project.validator.UserInfoDtoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor    // 의존성 주입, final 필드에 대해 생성자를 생성, @Autowired 대신 사용
 @Controller // 자동 응답기, controller 명시(@Component 포함): Bean 등록
 public class UserController {
     private final UserService userService;
+    private final UserInfoDtoValidator userInfoDtoValidator;
+
+    // 아이디 중복검사, 이메일 중복검사, 비밀번호 확인
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(userInfoDtoValidator); // 검증 결과를 errors 에 넣음
+    }
 
     // 회원가입 페이지 요청 (GET)
     @GetMapping("/user/signup")
-    public String signup(){
-        return "signup";    // 회원가입 페이지
+    public String signup(Model model){
+        model.addAttribute("UserInfoDto", new UserInfoDto()); // 빈 dto 객체 생성하여 변수 전달
+        return "user/signup";    // 회원가입 페이지 (초기)
     }
 
     // 회원가입 요청 (POST)
     @PostMapping("/user/signup")
-    public String registerUser(UserInfoDto userInfoDto){
-        return userService.registerUser(userInfoDto);       // 회원가입 성공 또는 오류 메세지
+    public String registerUser(@Valid UserInfoDto userInfoDto, Errors errors, Model model){
+        // 오류 처리
+        if (errors.hasErrors()){
+            model.addAttribute("UserInfoDto", userInfoDto); // dto 객체 생성하여 변수 전달
+            return "user/signup";   // 회원가입 페이지 이동 (오류 내용 포함)
+        }
+
+        // 정상 처리
+        userService.registerUser(userInfoDto);  // service 로 dto 전달
+        return "redirect:/login";   // 로그인 페이지 (templates/user/login.html) 이동
     }
 
 }
--- src/main/java/com/bellpro/mini_project/service/UserService.java
+++ src/main/java/com/bellpro/mini_project/service/UserService.java
@@ -5,6 +5,7 @@ import com.bellpro.mini_project.dto.UserInfoDto;
 import com.bellpro.mini_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor    // 의존성 주입, final 필드에 대해 생성자를 생성,  @Autowired 대신 사용
 @Service    // Service 명시(@Component 포함): Bean 등록
@@ -12,24 +13,14 @@ public class UserService {
     private final UserRepository userRepository;
 
     // 클라이언트 회원가입 요청 시
-    public String registerUser(UserInfoDto userInfoDto){
+    @Transactional  // 트랜잭션 처리 선언
+    public void registerUser(UserInfoDto userInfoDto){
         String username = userInfoDto.getUsername();
         String email = userInfoDto.getEmail();
-
-        // 아이디 중복 검사
-        if (userRepository.existsByUsername(username)){
-            return "중복된 아이디 입니다.";
-        }
-        // 이메일 중복 검사
-        if (userRepository.existsByEmail(email)){
-            return "중복된 이메일 입니다.";
-        }
 
         // 클라이언트가 요청한 사용자 객체 생성 후 DB 저장
         User user = new User(userInfoDto);
         userRepository.save(user);
 
-        // 정상 완료
-        return "회원가입을 완료했습니다.";
     }
 }
