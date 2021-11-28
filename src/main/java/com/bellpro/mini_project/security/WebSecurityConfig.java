bc767aff8713009b0ddf32f9e8344e2243b4112e
bellpro <kju1909020@gmail.com>
2021-11-28 17:18:40 +0900

    사용자 비밀번호 암호화 반영

---
+++ src/main/java/com/bellpro/mini_project/security/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.bellpro.mini_project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 비밀번호 암호화 사용
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // CSRF protection 을 비활성화
+        http.csrf().disable();
+        http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
+                // js 폴더를 login 없이 허용
+                .antMatchers("/js/**").permitAll()
+                // 회원 관리 처리 API 전부를 login 없이 허용
+                .antMatchers("/user/**").permitAll()
+                // 게시글 상세 페이지 로그인 없이 허용
+                .antMatchers("/board/detail/**").permitAll()
+                // 메인 페이지에서 게시글 검색 기능 로그인 없이 허용
+                .antMatchers("/board/search").permitAll()
+                // 글 목록 페이지 로그인 없이 허용
+                .antMatchers("/").permitAll() // 글 리스트 페이지 로그인 안해도 접속 가능하게
+                // 그 외 어떤 요청이든 '인증'
+                .anyRequest().authenticated()
+                .and()
+                // [로그인 기능]
+                .formLogin()
+                // 로그인 View 제공 (GET /user/login)
+                .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
+                // 로그인 처리 후 성공 시 URL
+                .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
+                .failureUrl("/user/login?error")
+                .permitAll()
+                .and()
+                // [로그아웃 기능]
+                .logout()
+                // 로그아웃 요청 처리 URL
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and()
+                .exceptionHandling();
+    }
+}
--- src/main/java/com/bellpro/mini_project/service/UserService.java
+++ src/main/java/com/bellpro/mini_project/service/UserService.java
@@ -4,16 +4,19 @@ import com.bellpro.mini_project.domain.User;
 import com.bellpro.mini_project.dto.UserInfoDto;
 import com.bellpro.mini_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor    // 의존성 주입, final 필드에 대해 생성자를 생성,  @Autowired 대신 사용
 @Service    // Service 명시(@Component 포함): Bean 등록
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     // 클라이언트 회원가입 요청 시
     public void registerUser(UserInfoDto userInfoDto){
+        userInfoDto.setPassword(passwordEncoder.encode(userInfoDto.getPassword()));   // 비밀번호 암호화
+
         // 클라이언트가 요청한 사용자 객체 생성 후 DB 저장
         User user = new User(userInfoDto);
         userRepository.save(user);
