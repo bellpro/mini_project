020d19afb70a57037abe54cade80ec415003c2f3
bellpro <kju1909020@gmail.com>
2021-11-28 23:39:04 +0900

    게시글 주서 변경

--- src/main/java/com/bellpro/mini_project/dto/BoardRequestDto.java
+++ src/main/java/com/bellpro/mini_project/dto/BoardRequestDto.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Getter // get 메소드 자동 생성
 @RequiredArgsConstructor    // 의존성 주입, final 필드에 대해 생성자를 생성,  @Autowired 대신 사용
 public class BoardRequestDto {
-    private final String author;    // 작성자
-    private final String title;     // 제목
-    private final String content;   // 내용
+    private final String author;    // 게시글 작성자
+    private final String title;     // 게시글 제목
+    private final String content;   // 게시글 내용
 }
