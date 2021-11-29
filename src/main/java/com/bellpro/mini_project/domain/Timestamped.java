89f3f0d786ac85d756ddb1c494ca625cc0826ecc
bellpro <kju1909020@gmail.com>
2021-11-29 11:38:52 +0900

    기능 완료

--- src/main/java/com/bellpro/mini_project/domain/Timestamped.java
+++ src/main/java/com/bellpro/mini_project/domain/Timestamped.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter // get 메소드 자동 생성
 @MappedSuperclass   // 상속 받은 클래스가 생성날짜/수정날짜 컬럼으로 사용하도록 설정
@@ -18,4 +21,9 @@ public abstract class Timestamped { // 추상화 클래스 설정
     @LastModifiedDate           // 수정날짜 설정
     private String modifiedAt;  // 수정날짜
 
+    @PrePersist
+    public void onPrePersist(){
+        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.modifiedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
 }
